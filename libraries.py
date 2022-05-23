40e3ec1063019ccf0d72ca6f5b787c11fb734878
Abramov-Dmitry <105738129+Abramov-Dmitry@users.noreply.github.com>
2022-05-23 03:29:28 +0300

    Add files via upload

--- libraries.py
+++ libraries.py
@@ -1,19 +1,31 @@
 import warnings
 from collections import Counter
-warnings.filterwarnings('ignore')
-
+# Библиотеки для манипуляции с данными
 import pandas as pd
 import numpy as np
+# Библиотеки для визуализации данных
 import seaborn as sns
-
 import matplotlib.pyplot as plt
 import plotly.express as px
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+# Предобработка
 from sklearn.decomposition import PCA
-from sklearn.metrics import recall_score, confusion_matrix, f1_score
-from sklearn.model_selection import (train_test_split, cross_val_score,
-                                     RandomizedSearchCV)
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import StandardScaler
+# Для понижения размерности данных, с помощью выбора K наиболее влиятельных
+#   признаков
+from sklearn.feature_selection import SelectKBest
+# Модели для обучения
+from sklearn.svm import LinearSVC, SVC
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+# Настройка и оценка
+from sklearn.model_selection import (train_test_split, cross_val_score,
+                                     RandomizedSearchCV, GridSearchCV)
+from sklearn.metrics import (recall_score, confusion_matrix, f1_score,
+                             accuracy_score, precision_score, roc_curve,
+                             roc_auc_score)
+# Для балансировки классов
 from imblearn.over_sampling import SMOTE
+
+warnings.filterwarnings('ignore')
